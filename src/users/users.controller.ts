f99509a60b61758d89ee775fc88906ee172feaab
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2022-12-04 15:16:07 +0100

    and
    added update user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,9 +10,7 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { HasRoles } from 'src/auth/decorators/permissions.decorator';
-import { Roles } from '@prisma/client';
-@HasRoles(Roles.ADMIN)
+
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
