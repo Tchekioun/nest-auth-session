47c6864c034338cd525bf80b9fefcb48ba362211
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2022-12-14 12:14:44 +0100

    add CreateClientDto to the documentation

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,10 +1,16 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger/dist';
 import { IsOptional, IsString, Length } from 'class-validator';
 export class CreateClientDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   denomination?: string;
+
+  @ApiProperty()
   @Length(10, 10)
   phoneNumber: string;
+
+  @ApiPropertyOptional()
   @IsOptional()
   @Length(10, 10)
   phoneNumber2?: string;
